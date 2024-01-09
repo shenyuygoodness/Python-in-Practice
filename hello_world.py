de97350d2c7e2a16a56590385d63ef81d4f62f43
shenyuygoodness <shenyuygoodness@gmail.com>
2024-01-09 21:30:22 +0100

    new

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,24 @@
 print("Hello World!")
 
+#variables
 message = "Hey there I want to become a python plug"
 print(message)
+
+#Data types
+#Strings
+"Hate is what you give"
+'period'
+
+# changing case in a string
+name = "Ada lovice"
+print(name.title()) # title() for capitalizing the string
+print(name.upper())  # upper() for all Uppercase letters
+print(name.lower())  # lowercase letters lower()
+
+# the above are all methods
+
+#Using variables in strings
+first = 'Ben'
+last = 'Ten'
+first_last = f"{first} {last}"
+print(first_last)
