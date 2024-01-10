151db8fa504d9e2be1007fb5d0f2682f7c40e9b2
shenyuygoodness <shenyuygoodness@gmail.com>
2024-01-10 14:28:46 +0100

    working with list

---
+++ exercise1.py
@@ -0,0 +1,8 @@
+names = ['KElly', 'Butlar', 'Desmond', 'Wes']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+
+message = f"hello {names[0]}."
+print(message)
---
+++ list.py
@@ -0,0 +1,25 @@
+bikes = ['host', 'redline', 'trek', 'bread']
+print(bikes)
+
+#Accessing elements in a list
+print(bikes[0])  # this is done by use of indexes
+
+# for more modification
+print(bikes[2].title())
+
+# acessing last elements in the list
+print(bikes[-1])
+# using individual values from a list
+message = f"My first bike was a {bikes[3].title()}."
+print(message)
+
+#Modifying elements in a list
+# changing elements in a list
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+# Adding an element to the list using append
+motorcycles.append('ducati')
+print(motorcycles)
