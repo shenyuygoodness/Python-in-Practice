99826e8c2cd2b2eea0fd7f0b394026aae1c4f669
shenyuygoodness <shenyuygoodness@gmail.com>
2024-01-10 17:29:58 +0100

    working with list 2

--- exercise1.py
+++ exercise1.py
@@ -6,3 +6,52 @@ print(names[3])
 
 message = f"hello {names[0]}."
 print(message)
+
+# Exercise 2
+guests = ['Noel', 'Ken', 'Wes', 'Shen', 'Bes']
+print(f"You are invited {guests[0]}")
+print(f"You are invited {guests[1]}")
+print(f"You are invited {guests[2]}")
+print(f"You are invited {guests[3]}")
+print(f"You are invited {guests[4]}")
+absent_guest = guests.pop(2)
+print(f"{absent_guest} can't make it ")
+guests.append('Mary')
+print(f"You are invited {guests[0]}")
+print(f"You are invited {guests[1]}")
+print(f"You are invited {guests[2]}")
+print(f"You are invited {guests[3]}")
+print(f"You are invited {guests[4]}")
+guests.insert(0, 'John')
+guests.insert(3, 'Pel')
+guests.append('Jack')
+print(f"You are invited {guests[0]}")
+print(f"You are invited {guests[1]}")
+print(f"You are invited {guests[2]}")
+print(f"You are invited {guests[3]}")
+print(f"You are invited {guests[4]}")
+print(f"You are invited {guests[5]}")
+print(f"You are invited {guests[6]}")
+print(f"You are invited {guests[7]}")
+
+print("\n Sorry guys, I can only invite 2 persons")
+removed_guest1 = guests.pop()
+print(f"{removed_guest1} Sorry!")
+removed_guest2 = guests.pop()
+print(f"{removed_guest2} Sorry!")
+removed_guest3 = guests.pop()
+print(f"{removed_guest3} Sorry!")
+removed_guest4 = guests.pop()
+print(f"{removed_guest4} Sorry!")
+removed_guest5 = guests.pop()
+print(f"{removed_guest5} Sorry!")
+removed_guest5 = guests.pop()
+print(f"{removed_guest5} Sorry!")
+print(guests)
+
+print(f"You are invited {guests[0]}")
+print(f"You are invited {guests[1]}")
+
+del guests[1]
+del guests[0]
+print(guests)
--- list.py
+++ list.py
@@ -23,3 +23,21 @@ print(motorcycles)
 # Adding an element to the list using append
 motorcycles.append('ducati')
 print(motorcycles)
+ 
+#Inserting elements into a list
+motorcycles.insert(2, 'benz')
+print(motorcycles)
+
+#Removing elements from a list
+#1) using del statement
+del motorcycles[1]
+print(motorcycles)
+#2) using pop() method
+popped_motocycle = motorcycles.pop()
+popped_motocycles = motorcycles.pop(0)
+print(motorcycles)
+print(popped_motocycle)
+
+# 3) removing by value using remove method
+motorcycles.remove('suzuki')
+print(motorcycles)
