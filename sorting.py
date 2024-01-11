d99791d5ad9683a7db1ecf000d96111fad685ee9
shenyuygoodness <shenyuygoodness@gmail.com>
2024-01-11 22:25:14 +0100

    sorting list

---
+++ sorting.py
@@ -0,0 +1,8 @@
+#permenent sorting with  the sort()  method
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars) #arranges it alphabetically
+
+# this could be done in reverse order
+cars.sort(reverse=True)
+print(cars)
