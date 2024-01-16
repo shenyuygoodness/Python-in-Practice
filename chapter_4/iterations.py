a0148e1b2011e0864d63e3ebb7a1d82fc861cbaa
shenyuygoodness <shenyuygoodness@gmail.com>
2024-01-16 20:03:17 +0100

    updated

--- chapter_4/iterations.py
+++ chapter_4/iterations.py
@@ -1,3 +1,8 @@
 magicians = ['alice', 'david', 'caro']
 for magician in magicians:
-    print(magician)
+    print(magician) 
+    print(f"{magician.title()}, that was a great trick\n")
+
+#printing ranges 
+for value in range(2, 6):
+    print(value)
