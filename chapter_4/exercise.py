c06f0de7be1bde80dea9cfc46d97735c081cc519
shenyuygoodness <shenyuygoodness@gmail.com>
2024-01-17 22:15:18 +0100

    updated chp4

---
+++ chapter_4/exercise.py
@@ -0,0 +1,15 @@
+for v in range(2, 22):
+    print(v)
+
+million = list(range(2, 1000001))
+#for value in million:
+    #print('hello')
+   # print(million)
+
+print(sum(million))
+odd_numbers = list(range(3, 30, 2))
+print(odd_numbers)
+
+
+cube = [v**3 for v in range(2, 9)]
+print(cube)
--- chapter_4/iterations.py
+++ chapter_4/iterations.py
@@ -6,3 +6,27 @@ for magician in magicians:
 #printing ranges 
 for value in range(2, 6):
     print(value)
+
+#printing lists
+numbers = list(range(2, 7))
+print(numbers)
+
+#getting list of numbers based on the adding a particular number
+even_numbers = list(range(2, 56, 5))
+print(even_numbers)
+
+rectangles = []
+for v in range(2, 23):
+    rectangle = v ** 2
+    rectangles.append(rectangle)
+    print(rectangles)
+
+# the above could be done in 2 lines
+rectangle = [value**2 for value in range(2, 22)]
+print(rectangle)
+
+#using methods
+
+print(max(rectangles))
+print(max(numbers))
+print(sum(even_numbers))
