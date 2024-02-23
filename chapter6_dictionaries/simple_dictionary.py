9504168fa78a75acaf2b354feba6640ec4fd07f4
shenyuygoodness <shenyuygoodness@gmail.com>
2024-02-23 19:30:44 +0100

    simple words

--- chapter6_dictionaries/simple_dictionary.py
+++ chapter6_dictionaries/simple_dictionary.py
@@ -1,3 +1,4 @@
 alien_0 = {'color': 'green', 'points': 5}
 print(alien_0['color'])
 print(alien_0['points'])
+# I'll be strong, fight, overcome, successful, bold, confident with God everything is possible
