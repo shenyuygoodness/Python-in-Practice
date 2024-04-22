4bd3f1dcc04529b59b454d5123abb88593e3c7de
shenyuygoodness <shenyuygoodness@gmail.com>
2024-04-22 22:52:12 +0100

    used for function for dictionary iteration

--- chapter6_dictionaries/simple_dictionary.py
+++ chapter6_dictionaries/simple_dictionary.py
@@ -1,4 +1,6 @@
-alien_0 = {'color': 'green', 'points': 5}
+alien_0 = {'color': {'green', 'red', 'purple'}, 'points': 5}
 print(alien_0['color'])
 print(alien_0['points'])
+for item in alien_0:
+    print(item)
 # I'll be strong, fight, overcome, successful, bold, confident with God everything is possible
